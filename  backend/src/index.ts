660e15e10f2dc3fe09ca3d9f6608a2695c54f505
amal <amalkanhangad333@gmail.com>
2024-08-17 17:14:58 +0530

    add JWT

---  backend/src/index.ts
+++  backend/src/index.ts
@@ -1,7 +1,8 @@
+import { Response } from './../node_modules/hono/dist/types/client/types.d';
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { Hono } from 'hono';
-import { sign } from 'hono/jwt'
+import { sign,verify } from 'hono/jwt'
 
 // Create the main Hono app
 const app = new Hono<{
@@ -12,12 +13,18 @@ const app = new Hono<{
 }>();
 
 app.use('/api/v1/blog/*', async (c, next) => {
-  // get the header
-  // verify the header
-  // if the header is correct, we need can proceed,
-  // if not, we return user a 403 status
-  await next()
-})
+
+  const header = c.req.header("authorization") || "";
+  // Bearer token
+  const token = header.split(" ")[1]
+  const response = await verify(header, c.env.JWT_SECRET)
+  if (response.id){
+    next()
+  }else{
+    c.status(403)
+    return c.json({error:"unauthorised"})
+  }
+  })
 
 app.post('/api/v1/signup', async (c) => {
 	const prisma = new PrismaClient({
